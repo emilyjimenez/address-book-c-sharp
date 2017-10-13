043176e26d2211fe0af1b0892cb0c0ab26ca65b8
Emily Wells Jimenez <emwellspdx@gmail.com>
2017-10-13 14:31:24 -0700

    finalize homecontroller and test functionality.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -12,5 +12,71 @@ namespace AddressBook.Controllers
     {
       return View();
     }
+
+    [HttpGet("/names")]
+    public ActionResult Names()
+    {
+      List<Name> allNames = Name.GetAll();
+      return View(allNames);
+    }
+
+    [HttpGet("/artists/new")]
+    public ActionResult NameForm()
+    {
+      return View();
+    }
+
+    [HttpPost("/names")]
+    public ActionResult AddName()
+    {
+      Name newName = new Name(Request.Form["contact-name"]);
+      List<Name> allNames = Name.GetAll();
+      return View("Names", allNames);
+    }
+
+    [HttpGet("/names/{id}")]
+    public ActionResult NameDetail(int id)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Name selectedName = Name.Find(id);
+      List<Address> nameAddresses = selectedName.GetNames();
+      model.Add("name", selectedName);
+      model.Add("addresses", nameAddresses);
+      return View(model);
+    }
+
+    [HttpGet("/names/{id}/addresses/new")]
+    public ActionResult NameAddressForm(int id)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Name selectedName = Name.Find(id);
+      List<Address> nameAddresses = selectedName.GetNames();
+      model.Add("name", selectedName);
+      model.Add("addresses", nameAddresses);
+      return View(model);
+    }
+
+    [HttpPost("/cds")]
+    public ActionResult AddAddress()
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Name selectedName = Name.Find(Int32.Parse(Request.Form["name-id"]));
+      List<Address> nameAddresses = selectedName.GetNames();
+      string addressStreet = Request.Form["address-street"];
+      string addressCityState = Request.Form["address-city-state"];
+      string addressZip = Request.Form["address-zip"];
+      Address newAddress = new Address(addressStreet, addressCityState, addressZip);
+      nameAddresses.Add(newAddress);
+      model.Add("name", selectedName);
+      model.Add("addresses", nameAddresses);
+      return View("NameDetail", model);
+    }
+
+    [HttpGet("/addresses/{id}")]
+    public ActionResult NameDetail(int id)
+    {
+      Address address = Address.Find(id);
+      return View(address);
+    }
   }
 }
