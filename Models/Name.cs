fc0afce50ed7b30849aed39b4276609ee5874be5
Emily Wells Jimenez <emwellspdx@gmail.com>
2017-10-13 10:57:26 -0700

    build name model.

---
+++ Models/Name.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+
+namespace AddressBook.Models
+{
+  public class Name
+  {
+    private static List<Name> _instances; = new List<Name> {};
+    private string _name;
+    private int _id;
+    private List<Name> _names;
+
+    public Name(string name)
+    {
+      _name = name;
+      _instances.Add(this);
+      _id = instances.Count;
+      _names = new List<Name> {};
+    }
+
+    public string GetName()
+    {
+      return _name;
+    }
+
+    public int GetId()
+    {
+      return _id;
+    }
+
+    public static List<Name> GetAll()
+    {
+      return _instances;
+    }
+
+    public static Name Find(int searchId)
+    {
+      return _instances[searchId-1];
+    }
+
+    public List<Name> GetNames()
+    {
+      return _names;
+    }
+
+    public void AddName(Name name)
+    {
+      _names.Add(name);
+    }
+  }
+}
